9e20b6798508ce9a79d193be40bbae8c0292cfac
Shufyan Ghani Khan <shufyan@Shufyans-MacBook-Pro.local>
2026-01-07 00:10:24 +0530

    Added backend.tf and updated pipeline code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+
+terraform {
+  required_version = ">= 1.4.0, < 2.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
